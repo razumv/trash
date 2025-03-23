9bbf955740db0ac5e80239ad2c499633fb536794
razumv <42905535+razumv@users.noreply.github.com>
2025-03-23 14:10:35 +0100

    Update restart.sh

--- layeredge/restart.sh
+++ layeredge/restart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 tmux kill-session -t risc_service 2>/dev/null; cd /root/light-node/risc0-merkle-service; tmux new-session -d -s risc_service "cargo build && cargo run"
+sleep 15
 sudo systemctl restart light-node
