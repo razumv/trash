8a321025c71ebe9af933e433e2d979a12ea85f0c
razumv <42905535+razumv@users.noreply.github.com>
2025-03-23 14:53:23 +0100

    Update restart.sh

--- layeredge/restart.sh
+++ layeredge/restart.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+export TERM=xterm
 
-# Убиваем tmux сессию
-tmux kill-session -t risc_service 2>/dev/null || true
-
-# Переходим в директорию
-cd /root/light-node/risc0-merkle-service
-
-# Запускаем сборку и запуск в tmux
-tmux new-session -d -s risc_service "cargo build && cargo run"
+# Проверяем доступность порта 3001
+if ! nc -z 127.0.0.1 3001; then
+    echo "[INFO] Порт 3001 недоступен. Запускаем risc_service..."
+    tmux new-session -d -s risc_service 'cd /root/light-node/risc0-merkle-service; /root/.cargo/bin/cargo build && /root/.cargo/bin/cargo run'
+else
+    echo "[OK] Порт 3001 доступен. Ничего не делаем."
+fi
 
 # Рандомная пауза от 1 до 60 минут перед рестартом light-node
 sleep_time=$((RANDOM % 60 + 1))
