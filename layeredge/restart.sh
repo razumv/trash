eb5a572619b83b744348a3d499d6b61c4cb7e36d
razumv <42905535+razumv@users.noreply.github.com>
2025-03-23 14:15:15 +0100

    Update restart.sh

--- layeredge/restart.sh
+++ layeredge/restart.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-tmux kill-session -t risc_service 2>/dev/null; cd /root/light-node/risc0-merkle-service; tmux new-session -d -s risc_service "cargo build && cargo run"
-sleep 15
+# Убиваем tmux сессию
+tmux kill-session -t risc_service 2>/dev/null || true
+
+# Переходим в директорию
+cd /root/light-node/risc0-merkle-service
+
+# Запускаем сборку и запуск в tmux
+tmux new-session -d -s risc_service "cargo build && cargo run"
+
+# Рандомная пауза от 1 до 60 минут перед рестартом light-node
+sleep_time=$((RANDOM % 60 + 1))
+echo "Спим $sleep_time минут(ы) перед systemctl restart..."
+sleep "${sleep_time}m"
+
+# Рестарт сервиса
 sudo systemctl restart light-node
