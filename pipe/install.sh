b8ff52fdc48f1a423f427a7626f2b11171ad9f20
razumv <42905535+razumv@users.noreply.github.com>
2025-06-02 19:33:40 +0200

    Update install.sh

--- pipe/install.sh
+++ pipe/install.sh
@@ -5,75 +5,261 @@ curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | b
 echo "-----------------------------------------------------------------------------"
 
 echo "-----------------------------------------------------------------------------"
+echo "Устанавливаем софт"
+echo "-----------------------------------------------------------------------------"
+sudo apt update -y &>/dev/null
+# curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh | bash &>/dev/null
+# curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/ufw.sh | bash &>/dev/null
+sudo apt install gawk bison build-essential manpages-dev ca-certificates -y &>/dev/null
+
+echo "-----------------------------------------------------------------------------"
 echo "Установка ноды"
 echo "-----------------------------------------------------------------------------"
 
-mkdir -p $HOME/pipe_backup
+USERNAME="popcache"
+LOGROTATE_FILE="/etc/logrotate.d/popcache"
 
-if systemctl list-units --type=service | grep -q "dcdnd.service"; then
-    cp -r $HOME/.permissionless $HOME/pipe_backup
+if id "$USERNAME" &>/dev/null; then
+    echo "Пользователь '$USERNAME' существует"
+else
+    sudo useradd -m -s /bin/bash "$USERNAME"
+    echo "Пользователь '$USERNAME' создан."
+fi
+sudo usermod -aG sudo "$USERNAME"
 
-    sudo systemctl stop dcdnd
-    sudo systemctl disable dcdnd
-    rm -rf /etc/systemd/system/dcdnd.service
-    rm -rf $HOME/opt/dcdn
-    rm -rf $HOME/.permissionless
+sudo tee /etc/sysctl.d/99-popcache.conf > /dev/null << "EOL"
+net.ipv4.ip_local_port_range = 1024 65535
+net.core.somaxconn = 65535
+net.ipv4.tcp_low_latency = 1
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_slow_start_after_idle = 0
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_wmem = 4096 65536 16777216
+net.ipv4.tcp_rmem = 4096 87380 16777216
+net.core.wmem_max = 16777216
+net.core.rmem_max = 16777216
+EOL
+
+sudo sysctl --system &>/dev/null
+
+sudo tee /etc/security/limits.d/popcache.conf > /dev/null << "EOL"
+*    hard nofile 65535
+*    soft nofile 65535
+EOL
+
+
+sudo mkdir -p /opt/popcache
+cd /opt/popcache
+
+ldd_version=$(ldd --version | head -n1 | awk '{print $NF}')
+
+if [[ "$ldd_version" == "2.39" ]]; then
+    while true; do
+        echo "Какой бинарник качаем, x64 или ARM? (1 - x64 / 2 - ARM)"
+        read -rp "Введите 1 или 2: " choice
+        case "$choice" in
+            1)
+                url="https://download.pipe.network/static/pop-v0.3.1-linux-x64.tar.gz"
+                wget "$url" &>/dev/null
+                tar -xf pop-v0.3.1-linux-x64.tar.gz &>/dev/null
+                break
+                ;;
+            2)
+                url="https://download.pipe.network/static/pop-v0.3.1-linux-arm64.tar.gz"
+                wget "$url" &>/dev/null
+                tar -xf pop-v0.3.1-linux-arm64.tar.gz &>/dev/null
+                break
+                ;;
+            *)
+                echo "Введите 1 или 2."
+                ;;
+        esac
+    done
+    chmod +x pop
+    pop_cmd="/opt/popcache/pop"
+else
+    echo "Билдим нужную версию glibc (2.39) - время ожидания 3-5 минут"
+    # ubuntu 22.04 compatability
+    mkdir -p /opt/glibc-build
+    cd /opt/glibc-build
+    
+    # installing glibc 2.39
+    wget http://ftp.gnu.org/gnu/libc/glibc-2.39.tar.gz &>/dev/null
+    tar -xf glibc-2.39.tar.gz &>/dev/null
+    mkdir glibc-2.39-build glibc-2.39-install
+    cd glibc-2.39-build
+    ../glibc-2.39/configure --prefix=/opt/glibc-build/glibc-2.39-install &>/dev/null
+    make -j$(nproc) &>/dev/null
+    make install &>/dev/null
+
+    sudo chown -R root:root /opt/glibc-build
+    chmod -R a+rx /opt/glibc-build
+    
+    # downloading pop binary
+    cd /opt/popcache
+    wget https://download.pipe.network/static/pop-v0.3.1-linux-x64.tar.gz &>/dev/null
+    tar -xf pop-v0.3.1-linux-x64.tar.gz &>/dev/null
+    chmod +x pop
+    
+    pop_cmd="/opt/glibc-build/glibc-2.39-install/lib/ld-linux-x86-64.so.2 --library-path \"/opt/glibc-build/glibc-2.39-install/lib:/usr/lib/x86_64-linux-gnu/\" /opt/popcache/pop"
 fi
 
-# echo "Введите POP URL: "
-# read POP
+# read -rp "Введите Solana public key: " solana_addr
+# read -rp "Введите инвайт код: " invite_code
+# read -rp "Введите Pop Name(имя) для конфига: " pop_name
+# read -rp "Введите Pop Location(Страну) для конфига: " pop_location
 
-# echo "Введите адрес кошелька соланы: "
-# read PUB_KEY
-
-# echo "Введите реферальный код или нажмите ENTER: "
-# read REF
-
-cd $HOME
-sudo mkdir -p $HOME/opt/dcdn/download_cache
-
-sudo wget -O $HOME/opt/dcdn/pop "$POP"
-
-sudo chmod +x $HOME/opt/dcdn/pop
-sudo ln -s $HOME/opt/dcdn/pop /usr/local/bin/pop -f
-
-if [ -n "$REF" ]; then
-    cd $HOME/opt/dcdn/
-    ./pop --signup-by-referral-route $REF
+if [ -e /opt/popcache/config.json ]; then
+  # read -rp "config.json уже существует. Перезаписать? [y/N]: " confirm
+  # if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
+  #   echo "Пропускаем запись в файл config.json."
+  # else
+    cat > /opt/popcache/config.json <<EOF
+{
+  "pop_name": "$pop_name",
+  "pop_location": "$pop_location",
+  "invite_code": "$invite_code",
+  "server": {
+    "host": "0.0.0.0",
+    "port": 443,
+    "http_port": 80,
+    "workers": 40
+  },
+  "cache_config": {
+    "memory_cache_size_mb": 4096,
+    "disk_cache_path": "./cache",
+    "disk_cache_size_gb": 100,
+    "default_ttl_seconds": 86400,
+    "respect_origin_headers": true,
+    "max_cacheable_size_mb": 1024
+  },
+  "api_endpoints": {
+    "base_url": "https://dataplane.pipenetwork.com"
+  },
+  "identity_config": {
+    "node_name": "your-node-name",
+    "name": "Your Name",
+    "email": "your.email@example.com",
+    "website": "https://your-website.com",
+    "discord": "your_discord_username",
+    "telegram": "your_telegram_handle",
+    "solana_pubkey": "$solana_addr"
+  }
+}
+EOF
+  fi
+else
+  cat > /opt/popcache/config.json <<EOF
+{
+  "pop_name": "$pop_name",
+  "pop_location": "$pop_location",
+  "invite_code": "$invite_code",
+  "server": {
+    "host": "0.0.0.0",
+    "port": 443,
+    "http_port": 80,
+    "workers": 40
+  },
+  "cache_config": {
+    "memory_cache_size_mb": 4096,
+    "disk_cache_path": "./cache",
+    "disk_cache_size_gb": 100,
+    "default_ttl_seconds": 86400,
+    "respect_origin_headers": true,
+    "max_cacheable_size_mb": 1024
+  },
+  "api_endpoints": {
+    "base_url": "https://dataplane.pipenetwork.com"
+  },
+  "identity_config": {
+    "node_name": "your-node-name",
+    "name": "Your Name",
+    "email": "your.email@example.com",
+    "website": "https://your-website.com",
+    "discord": "your_discord_username",
+    "telegram": "your_telegram_handle",
+    "solana_pubkey": "$solana_addr"
+  }
+}
+EOF
 fi
 
-sudo tee /etc/systemd/system/pop.service > /dev/null << EOF
+
+sudo mkdir -p /opt/popcache/logs
+sudo chown -R popcache:popcache /opt/popcache
+
+SERVICE_NAME="popcache.service"
+SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME"
+# удаляем сервис если уже стоит
+if systemctl list-units --type=service --all | grep -q "$SERVICE_NAME"; then
+    sudo systemctl stop "$SERVICE_NAME"
+    sudo systemctl disable "$SERVICE_NAME"
+    if [ -f "$SERVICE_FILE" ]; then
+        sudo rm "$SERVICE_FILE"
+    fi
+    sudo systemctl daemon-reload
+    echo "Существующий $SERVICE_NAME удален."
+fi
+
+echo "Создаем systemd сервис popcache."
+
+sudo tee /etc/systemd/system/popcache.service > /dev/null <<EOL
 [Unit]
-Description=Pipe POP Node Service
+Description=POP Cache Node
 After=network.target
-Wants=network-online.target
 
 [Service]
-ExecStart=$HOME/opt/dcdn/pop --ram=4 --pubKey $PUB_KEY --max-disk 100 --cache-dir $HOME/opt/dcdn/download_cache
+Type=simple
+User=root
+Group=root
+WorkingDirectory=/opt/popcache
+ExecStart=$pop_cmd
 Restart=always
 RestartSec=5
-LimitNOFILE=65536
-LimitNPROC=4096
-StandardOutput=journal
-StandardError=journal
-SyslogIdentifier=dcdn-node
-WorkingDirectory=$HOME/opt/dcdn
-AmbientCapabilities=CAP_NET_BIND_SERVICE
-CapabilityBoundingSet=CAP_NET_BIND_SERVICE
+LimitNOFILE=65535
+StandardOutput=append:/opt/popcache/logs/stdout.log
+StandardError=append:/opt/popcache/logs/stderr.log
+Environment=POP_CONFIG_PATH=/opt/popcache/config.json
+Environment=POP_INVITE_CODE=$invite_code
 
 [Install]
 WantedBy=multi-user.target
-EOF
+EOL
 
+sudo systemctl daemon-reexec
 sudo systemctl daemon-reload
-sudo systemctl enable pop
-sudo systemctl start pop
 
-cp $HOME/opt/dcdn/node_info.json $HOME/pipe_backup/node_info.json
+sudo systemctl enable popcache
+sudo service popcache start
+echo "Сервис создан и запущен."
+
+sudo touch "$LOGROTATE_FILE"
+sudo chmod 0640 "$LOGROTATE_FILE"
+sudo chown popcache:popcache "$LOGROTATE_FILE"
+
+sudo bash -c "cat > $LOGROTATE_FILE << EOL
+/opt/popcache/logs/*.log {
+    daily
+    missingok
+    rotate 14
+    compress
+    delaycompress
+    notifempty
+    create 0640 popcache popcache
+    sharedscripts
+    postrotate
+        systemctl reload popcache >/dev/null 2>&1 || true
+    endscript
+}
+EOL"
+sudo mkdir -p /opt/popcache/logs
+sudo chown -R "$USER:$GROUP" /opt/popcache/logs
+echo "Ротация логов настроена."
 
 echo "-----------------------------------------------------------------------------"
 echo "Проверка логов"
-echo "journalctl -n 100 -f -u pop -o cat"
+echo "tail -f /opt/popcache/logs/stdout.log"
+echo "sudo journalctl -u popcache -f"
 echo "-----------------------------------------------------------------------------"
 echo "Wish lifechange case with DOUBLETOP"
 echo "-----------------------------------------------------------------------------"
